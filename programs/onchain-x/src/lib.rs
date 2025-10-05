694713d8cdd695d048f12e9492891947e201d191
siyasjr <siyasjr@gmail.com>
2025-10-05 23:26:29 +0530

    Calculated  needed storage for Tweet struct

--- programs/onchain-x/src/lib.rs
+++ programs/onchain-x/src/lib.rs
@@ -11,4 +11,12 @@ pub struct Tweet
     content: String, 
 }
 
+const DISCRIMINATOR_LENGTH: usize = 4;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const  TIME_STAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_TOPIC_LENGTH: usize = 200; //50 * 4 
+const MAX_CONTENT_LENGTH: usize = 1120; //280 * 4
+
+
 
