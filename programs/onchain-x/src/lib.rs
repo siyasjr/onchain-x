94851e2149f0d0c86b3e868ad55d7b81422f957c
siyasjr <siyasjr@gmail.com>
2025-10-14 22:19:27 +0530

    fixed deprecated imports and small bugs

--- programs/onchain-x/src/lib.rs
+++ programs/onchain-x/src/lib.rs
@@ -1,92 +1,68 @@
 use anchor_lang::prelude::*;
-use anchor_lang::system_program;
 
-declare_id!("BCHAprg6MuE55yWqi75jYXUnWEvCiJrrwWXp1PdGGdqX");
-
+declare_id!("2BDbYV1ocs2S1PsYnd5c5mqtdLWGf5VbCYvf28rs9LGj");
 
 #[program]
-pub mod onchain_x {
+pub mod solana_twitter {
+    use super::*;
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get().unwrap();
 
-  use crate::ErrorCode;
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into())
+        }
 
-use super::*; 
-  pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
-    let tweet = &mut ctx.accounts.tweet;
-    let author = &ctx.accounts.author;
-    let clock = Clock::get()?;
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into())
+        }
 
-    if topic.chars().count() >=50 {
-      
-      return Err(ErrorCode::TopicTooLong.into());
+        tweet.author = *author.key;
+        tweet.timestamp = clock.unix_timestamp;
+        tweet.topic = topic;
+        tweet.content = content;
 
+        Ok(())
     }
-
-    if content.chars().count() >= 280 {
-      
-      return Err(ErrorCode::ContentTooLong.into());
-    }
-
-
-    tweet.author = author.key();
-    tweet.topic = topic;
-    tweet.content = content; 
-    tweet.timestamp = clock.unix_timestamp;
-
-    Ok(())
-
-  }
-  
 }
 
 #[derive(Accounts)]
-pub struct  SendTweet<'info> {
-
-  #[account(init, payer = author, space = Tweet::LEN)]
-  pub tweet: Account<'info, Tweet>,
-  #[account(mut)]
-  pub author: Signer<'info>,
-
-  #[account(address = system_program::ID)]
-  pub system_program: Program<'info, System>
+pub struct SendTweet<'info> {
+    #[account(init, payer = author, space = Tweet::LEN)]
+    pub tweet: Account<'info, Tweet>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
-
 #[account]
-pub struct Tweet
- {
+pub struct Tweet {
     pub author: Pubkey,
     pub timestamp: i64,
     pub topic: String,
-    pub content: String, 
-}
-
-
-impl Tweet {
-    pub const LEN: usize = 
-      DISCRIMINATOR_LENGTH 
-    + PUBLIC_KEY_LENGTH
-    + TIME_STAMP_LENGTH
-    + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH
-    +STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH;
+    pub content: String,
 }
 
 const DISCRIMINATOR_LENGTH: usize = 8;
 const PUBLIC_KEY_LENGTH: usize = 32;
-const  TIME_STAMP_LENGTH: usize = 8;
-const STRING_LENGTH_PREFIX: usize = 4;
-const MAX_TOPIC_LENGTH: usize = 200; //50 * 4 
-const MAX_CONTENT_LENGTH: usize = 1120; //280 * 4
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars max.
+const MAX_CONTENT_LENGTH: usize = 280 * 4; // 280 chars max.
 
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
+}
 
 #[error_code]
 pub enum ErrorCode {
-  #[msg("The provided topic should be 50 character long maximum")]
-  TopicTooLong,
-
-  #[msg("The provided content should be 280 characters long maximum")]
-  ContentTooLong,
-    
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
 }
-
-
-
